df011c3b1d25cbfd149391fdde5b4b174472fcea
Morenike Oyewole <herroyalpianist@gmail.com>
2022-10-28 20:47:33 +0100

    Edit pfoile section to implement image hover state

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
-import { SmallHeading } from '../Typography/Typography';
+import React from "react";
+import { SmallHeading } from "../Typography/Typography";
 import "./Profile.scss";
+import dp from "../../Assets/profile.jpg";
 
 const Profile = () => {
+
     return (
-        <div className='profile'>
-            <div className='profile__img'>
-                <img src='https://avatars.githubusercontent.com/u/65955563?v=4' alt='profile' id='profile__img' />
+        <div className="profile">
+            <div className="profile__img">
+                <img src={dp} alt="profile" id="profile__img" />
             </div>
-            <div className='profile__details'>
-                <SmallHeading title='Mo_renike_' id="twitter" />
-                <SmallHeading title='mo-renike' id="slack" />
+            <div className="profile__details">
+                <SmallHeading title="Mo_renike_" id="twitter" />
+                <SmallHeading title="mo-re-ni-ke" id="slack" />
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Profile;
