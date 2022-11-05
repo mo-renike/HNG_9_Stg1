e0f09f4c94115ed0e583418250cc4eb0c6e587db
Morenike Oyewole <herroyalpianist@gmail.com>
2022-11-05 12:26:30 +0100

    bug: fix form validations

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -9,14 +9,15 @@ const Contact = () => {
         fname: "",
         lname: "",
         email: "",
-        message: ""
+        message: "",
     });
 
     const [errors, setErrors] = React.useState({
         firstname: "",
         lastname: "",
         email: "",
-        message: ""
+        message: "",
+        check: "",
     });
 
     const handleChange = (e) => {
@@ -28,18 +29,30 @@ const Contact = () => {
     };
     const validate = (form) => {
         let errors = {};
-        // COMMENTED THESE ONES OUT COZ THEY NOT IN THE DESIGN
-        // if (!form.fname) {
-        //     errors.firstname = "First name is required";
-        // }
-        // if (!form.lname) {
-        //     errors.lastname = "Last name is required";
-        // }
-        // if (!form.email) {
-        //     errors.email = "Email is required";
-        // } else if (!/\S+@\S+\.\S+/.test(form.email)) {
-        //     errors.email = "Email address is invalid";
-        // }
+        if (!form.fname) {
+            errors.firstname = "First name is required";
+            document.getElementById("first_name").classList.add("input_error");
+        } else {
+            errors.message = "";
+            document.getElementById("first_name").classList.remove("input_error");
+        }
+        if (!form.lname) {
+            errors.lastname = "Last name is required";
+            document.getElementById("last_name").classList.add("input_error");
+        } else {
+            errors.message = "";
+            document.getElementById("last_name").classList.remove("input_error");
+        }
+        if (!form.email) {
+            errors.email = "Email is required";
+            document.getElementById("email").classList.add("input_error");
+        } else if (!/\S+@\S+\.\S+/.test(form.email)) {
+            errors.email = "Email address is invalid";
+            document.getElementById("email").classList.remove("input_error");
+        } else {
+            errors.message = "";
+            document.getElementById("email").classList.remove("input_error");
+        }
         if (!form.message) {
             errors.message = "Message is required";
             document.getElementById("message").classList.add("input_error");
@@ -47,8 +60,16 @@ const Contact = () => {
             errors.message = "";
             document.getElementById("message").classList.remove("input_error");
         }
+        // if  checkbox is not checked
+        if (!document.getElementById("checkbox").checked) {
+            errors.check = "Please check the box";
+            document.getElementById("checkbox").classList.add("input_error");
+        } else {
+            errors.check = "";
+        }
         return errors;
     };
+
     const handleSubmit = (e) => {
         e.preventDefault();
         handleChange(e);
@@ -70,7 +91,6 @@ const Contact = () => {
     };
 
 
-
     return (
         <div className="contact">
             {/* build form */}
@@ -78,35 +98,67 @@ const Contact = () => {
             <form onSubmit={handleSubmit} noValidate className="contact__form">
                 <div className="contact__header">
                     <Heading title="Contact Me" />
-                    <p>Hi there, contact me to ask me about anything you have in mind</p>
+                    <p>
+                        Hi there, contact me to ask me about anything you have in mind
+                    </p>
                 </div>
                 <div className="d-f">
                     <div className="contact__form__group">
                         <label htmlFor="fname">First Name</label>
-                        <input type="text" name="fname" id="first_name" value={form.fname} onChange={handleChange} />
+                        <input
+                            type="text"
+                            name="fname"
+                            id="first_name"
+                            value={form.fname}
+                            onChange={handleChange}
+                        />
                         {errors.firstname && <p className="error">{errors.firstname}</p>}
                     </div>
                     <div className="contact__form__group">
                         <label htmlFor="lname">Last Name</label>
-                        <input type="text" name="lname" id="last_name" value={form.lname} onChange={handleChange} />
+                        <input
+                            type="text"
+                            name="lname"
+                            id="last_name"
+                            value={form.lname}
+                            onChange={handleChange}
+                        />
                         {errors.lastname && <p className="error">{errors.lastname}</p>}
-                    </div></div>
+                    </div>
+                </div>
                 <div className="contact__form__group">
                     <label htmlFor="email">Email</label>
-                    <input type="email" name="email" id="email" value={form.email} onChange={handleChange} />
+                    <input
+                        type="email"
+                        name="email"
+                        id="email"
+                        value={form.email}
+                        onChange={handleChange}
+                    />
                     {errors.email && <p className="error">{errors.email}</p>}
                 </div>
                 <div className="contact__form__group">
                     <label htmlFor="message">Message</label>
-                    <textarea name="message" id="message" value={form.message} onChange={handleChange} />
+                    <textarea
+                        name="message"
+                        id="message"
+                        value={form.message}
+                        onChange={handleChange}
+                    />
                     {errors.message && <p className="error">{errors.message}</p>}
                 </div>
                 <div className="contact__form__group-check">
                     <input type="checkbox" name="checkbox" id="checkbox" />
-                    <label htmlFor="checkbox">You agree to providing your data to Morenike Oyewole who may contact you</label>
+                    <label htmlFor="checkbox">
+                        You agree to providing your data to Morenike Oyewole who may
+                        contact you
+                    </label>
+                    {errors.check && <p className="error">{errors.check}</p>}
                 </div>
                 <div className="contact__form__group">
-                    <button disabled={""} id="btn__submit" type="submit">Submit</button>
+                    <button disabled={""} id="btn__submit" type="submit">
+                        Submit
+                    </button>
                 </div>
             </form>
         </div>
